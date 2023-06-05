e5454399d83b62623065321c092a1605794b58ba
bingual <bingual76@gmail.com>
2023-06-05 12:09:35 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'color-text-blue': '#1b5cfe',
+                'color-header-bl': '#24292f',
+                'default-border': '#e5e5e5',
+            },
+            fontFamily: {
+                pangolin: 'var(--pangolin)',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
+    },
+    plugins: [],
+};
